cb697f9687bde0be99006068a07294a07ec24a3d
Branden Schrenk <schrenk.br@gmail.com>
2022-03-21 10:11:25 -0400

    fix: font size on home too small

--- src/site/pages/index/Index.jsx
+++ src/site/pages/index/Index.jsx
@@ -8,21 +8,21 @@ function Index() {
             <Helmet>
                 <title>Branden Schrenk | Home</title>
                 <link rel="canonical" href="https://www.schrenk.dev/" />
-                <meta name="description" content="A website all about Branden Schrenk to demo and talk about his work and skills" />
-                <meta name="keywords" content="branden,schrenk,website,developer,engineer,application" />
+                <meta name="description" content="A website to learn about Branden Schrenk. This is a place Branden links to his work and skills" />
+                <meta name="keywords" content="branden,schrenk,react,frontend,website,developer,engineer,application,full,stack,cincinnati" />
 
                 {/* OpenGraph Tags */}
                 <meta property="og:title" content="Branden Schrenk | Home" />
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://www.schrenk.dev/" />
                 <meta property="og:image" content="https://www.schrenk.dev/android-chrome-192x192.png" />
-                <meta property="og:description" content="A website all about Branden Schrenk to demo and talk about his work and skills" />
+                <meta property="og:description" content="A website to learn about Branden Schrenk. This is a place Branden links to his work and skills" />
             </Helmet>
             <div>
                 <h2>Hello,</h2>
-                <p>My name is Branden Schrenk</p>
+                <h3>My name is <span>Branden Schrenk</span></h3>
                 <a href="/about">
-                    <button className="btnPrimary">Get to Know Me</button>
+                    <button className="btnPrimary">Learn About Branden</button>
                 </a>
             </div>
         </div>
