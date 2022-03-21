04701dcd69874f9deb209092924245129f34edf2
Branden Schrenk <schrenk.br@gmail.com>
2022-03-21 10:43:44 -0400

    feat: add contact page schema

--- src/site/pages/contact/Contact.jsx
+++ src/site/pages/contact/Contact.jsx
@@ -17,6 +17,17 @@ function Contact() {
                 <meta property="og:url" content="https://www.schrenk.dev/contact" />
                 <meta property="og:image" content="https://www.schrenk.dev/android-chrome-192x192.png" />
                 <meta property="og:description" content="Get in touch with Branden Schrenk today." />
+
+                {/* ContactPage SCHEMA */}
+                <script type="application/ld+json">{`
+                {
+                    "@context": "https://schema.org",
+                    "@type": "ContactPage",
+                    "name": "Branden Schrenk | Contact",
+                    "keywords": "contact,branden,schrenk",
+                    "description": "Get in touch with Branden Schrenk today."
+                }
+                `}</script>
             </Helmet>
             <div>
                 <iframe title="Contact Branden" className='contact-frame' src="https://docs.google.com/forms/d/e/1FAIpQLSe5YFiHAXaJZHoUl9dFjt1psb-6hiqgC_sdihT3abBi8lK0gQ/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
